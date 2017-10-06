4cf8b7ab49e1f5f50cc6e41590afcc215510025e
ReginaRR <Regiregi6@mail.ru>
2017-10-06 04:50:21 +0300

    Add files via upload

--- cgi/web.py
+++ cgi/web.py
@@ -1,14 +1,7 @@
-"""
-Реализация веб-сервера на языке Python, способная запускать серверные
-CGI-сценарии на языке Python; обслуживает файлы и сценарии в текущем
-рабочем каталоге; сценарии на языке Python должны находиться в каталоге
-webdir\cgi-bin или webdir\htbin;
-"""
 import os, sys
 from http.server import HTTPServer, CGIHTTPRequestHandler
 cgi = '.' # место, где находятся файлы html и подкаталог cgi-bin
-port = 80 # по умолчанию http://localhost/, иначе используйте
- # http://localhost:xxxx/
+port = 80 
 os.chdir(cgi) # перейти в корневой каталог HTML
 srvraddr = ("", port) # имя хоста и номер порта
 srvrobj = HTTPServer(srvraddr, CGIHTTPRequestHandler)
